485e4a7dfdb7bef59fd92a0fa649a1551cfe8814
Kdfrizale <kyle.frizzell.14@cnu.edu>
2017-10-06 14:12:41 -0400

    it's Workinggit add *..Now to add receiving input

--- src/mymotion_planning_api_tutorial.cpp
+++ src/mymotion_planning_api_tutorial.cpp
@@ -1,39 +1,3 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2012, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta */
-
 #include <pluginlib/class_loader.h>
 #include <ros/ros.h>
 
@@ -127,20 +91,9 @@ int main(int argc, char** argv)
   // specifying the desired pose of the end-effector as input.
   planning_interface::MotionPlanRequest req;
   planning_interface::MotionPlanResponse res;
+
   geometry_msgs::PoseStamped pose;
   pose.header.frame_id = "m1n6a200_link_base";
-  /*pose.pose.position.x = 0.0374196;
-  pose.pose.position.y = -0.12015;
-  pose.pose.position.z = 0.733757;
-  pose.pose.orientation.w = 1.0;
-  */
-  /*pose.pose.position.x = 0.103006;
-  pose.pose.position.y = -0.182455;
-  pose.pose.position.z = 0.785472;
-  pose.pose.orientation.x = 0.633357;
-  pose.pose.orientation.y = 0.314428;
-  pose.pose.orientation.z = 0.633351;
-  pose.pose.orientation.w = 0.31442;*/
   pose.pose.position.x = 0.305735; //pose is finger tip2
   pose.pose.position.y = 0.196963;
   pose.pose.position.z = 0.663391;
@@ -149,7 +102,7 @@ int main(int argc, char** argv)
   pose.pose.orientation.z = 0.623522;
   pose.pose.orientation.w = 0.333571;
 
-geometry_msgs::PoseStamped poseLink6;
+  geometry_msgs::PoseStamped poseLink6;
   poseLink6.header.frame_id = "m1n6a200_link_base";
   poseLink6.pose.position.x = 0.308219;
   poseLink6.pose.position.y = 0.0494515;
@@ -173,11 +126,10 @@ geometry_msgs::PoseStamped poseLink6;
   req.group_name = "chainArm";
   moveit_msgs::Constraints pose_goal =
       kinematic_constraints::constructGoalConstraints("m1n6a200_link_finger_tip_2", pose, tolerance_pose, tolerance_angle);
-
   req.goal_constraints.push_back(pose_goal);
+
   moveit_msgs::Constraints pose_goal2 =
       kinematic_constraints::constructGoalConstraints("m1n6a200_link_6", poseLink6, tolerance_pose, tolerance_angle);
-
   req.goal_constraints.push_back(pose_goal2);
 
   planning_interface::PlanningContextPtr context =
@@ -203,9 +155,10 @@ geometry_msgs::PoseStamped poseLink6;
   display_trajectory.trajectory.push_back(response.trajectory);
   display_publisher.publish(display_trajectory);
 
-  sleep_time.sleep();
+  ros::WallDuration waitForPlan_time(3.0);
+  waitForPlan_time.sleep();
 
-  //----------------------------------------Robot has planned to the first motion----------
+  //-------------Robot has planned to the first motion------------------------------------
   robot_state::RobotState& robot_state = planning_scene->getCurrentStateNonConst();
   planning_scene->setCurrentState(response.trajectory_start);
   const robot_state::JointModelGroup* joint_model_group = robot_state.getJointModelGroup("chainArm");
@@ -265,67 +218,7 @@ geometry_msgs::PoseStamped poseLink6;
   display_trajectory.trajectory.push_back(response.trajectory);
   display_publisher.publish(display_trajectory);
 
-  sleep_time.sleep();
-
-
-  // Joint Space Goals
-  // ^^^^^^^^^^^^^^^^^
-  /* First, set the state in the planning scene to the final state of the last plan */
-/*  robot_state::RobotState& robot_state = planning_scene->getCurrentStateNonConst();
-  planning_scene->setCurrentState(response.trajectory_start);
-  const robot_state::JointModelGroup* joint_model_group = robot_state.getJointModelGroup("right_finger");
-  robot_state.setJointGroupPositions(joint_model_group, response.trajectory.joint_trajectory.points.back().positions);
-
-  // Now, setup a joint space goal
-  robot_state::RobotState goal_state(robot_model);
-  std::vector<double> joint_values(2, 1.0);
-  joint_values[0] = 1.0;
-  joint_values[1] = 1.0;
-  //joint_values[3] = -0.2;
-  //joint_values[5] = -0.15;
-  const std::vector<std::string> &joint_names = joint_model_group->getVariableNames();
-  for (std::size_t i = 0; i < joint_names.size(); ++i)
-  {
-    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-  }
-  goal_state.setJointGroupPositions(joint_model_group, joint_values);
-  moveit_msgs::Constraints joint_goal = kinematic_constraints::constructGoalConstraints(goal_state, joint_model_group);
-  req.goal_constraints.clear();
-  req.goal_constraints.push_back(joint_goal);
-
-  // Call the planner and visualize the trajectory
-  // Re-construct the planning context
-  context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
-  //Call the Planner
-  context->solve(res);
-  // Check that the planning was successful
-  if (res.error_code_.val != res.error_code_.SUCCESS)
-  {
-    ROS_ERROR("Could not compute plan successfully");
-    return 0;
-  }
-  //Visualize the trajectory
-  ROS_INFO("Visualizing the trajectory");
-  res.getMessage(response);
-  display_trajectory.trajectory_start = response.trajectory_start;
-  display_trajectory.trajectory.push_back(response.trajectory);
-
-  //Now you should see two planned trajectories in series
-  display_publisher.publish(display_trajectory);
-
-  // We will add more goals. But first, set the state in the planning
-    // scene to the final state of the last plan
-  robot_state.setJointGroupPositions(joint_model_group, response.trajectory.joint_trajectory.points.back().positions);
-
-  //Now, we go back to the first goal
-  req.goal_constraints.clear();
-  req.goal_constraints.push_back(pose_goal);
-  context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
-  context->solve(res);
-  res.getMessage(response);
-  display_trajectory.trajectory.push_back(response.trajectory);
-  display_publisher.publish(display_trajectory);
-*/
+  waitForPlan_time.sleep();
 
   // END_TUTORIAL
   sleep_time.sleep();
