0bcd43a1a4c0f7338a5c259af6f311a7365841a1
Kdfrizale <kyle.frizzell.14@cnu.edu>
2017-10-07 02:07:19 -0400

    added custom Msg, subsriber, dummyTestPublisher.. working but needs to be better about planning from the previous state...

---
+++ src/myDummyPub.cpp
@@ -0,0 +1,74 @@
+#include "ros/ros.h"
+#include <mytutorialPkg/HandStampedPose.h>
+#include <geometry_msgs/PoseStamped.h>
+//#include "sensor_msgs/JointState.h"
+#include <cstdlib>
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc,argv, "myDummyPub");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<mytutorialPkg::HandStampedPose>("/handPoseTopic", 10);
+  ros::Rate loop_rate(10);
+  int count =0;
+
+  geometry_msgs::PoseStamped sensedPoseTip2;//Right Finger tip---Demo data
+  sensedPoseTip2.header.frame_id = "m1n6a200_link_base";
+  sensedPoseTip2.pose.position.x = 0.305735;
+  sensedPoseTip2.pose.position.y = 0.196963;
+  sensedPoseTip2.pose.position.z = 0.663391;
+  sensedPoseTip2.pose.orientation.x = 0.623481;
+  sensedPoseTip2.pose.orientation.y = 0.3335;
+  sensedPoseTip2.pose.orientation.z = 0.623522;
+  sensedPoseTip2.pose.orientation.w = 0.333571;
+
+  geometry_msgs::PoseStamped sensedPoseLink6;//wrist --- Demo Data
+  sensedPoseLink6.header.frame_id = "m1n6a200_link_base";
+  sensedPoseLink6.pose.position.x = 0.308219;
+  sensedPoseLink6.pose.position.y = 0.0494515;
+  sensedPoseLink6.pose.position.z = 0.727438;
+  sensedPoseLink6.pose.orientation.x = 0.615034;
+  sensedPoseLink6.pose.orientation.y = 0.0;
+  sensedPoseLink6.pose.orientation.z = 0.0;
+  sensedPoseLink6.pose.orientation.w = 0.788501;
+
+  geometry_msgs::PoseStamped sensedPoseTip1;//Left Finger tip---Demo data
+  sensedPoseTip1.header.frame_id = "m1n6a200_link_base";
+  sensedPoseTip1.pose.position.x = 0.30573;
+  sensedPoseTip1.pose.position.y = 0.211976;
+  sensedPoseTip1.pose.position.z = 0.723024;
+  sensedPoseTip1.pose.orientation.x = 0.648451;
+  sensedPoseTip1.pose.orientation.y = 0.282066;
+  sensedPoseTip1.pose.orientation.z = -0.648369;
+  sensedPoseTip1.pose.orientation.w = -0.282078;
+
+  while (ros::ok()){
+    sensedPoseTip2.pose.position.y += 0.001;
+    sensedPoseLink6.pose.position.y += 0.001;
+    sensedPoseTip1.pose.position.y += 0.001;
+
+    mytutorialPkg::HandStampedPose msg;
+    msg.poseTip2 = sensedPoseTip2;
+    msg.poseLink6 = sensedPoseLink6;
+    msg.poseTip1 = sensedPoseTip1;
+
+  //  sensor_msgs::JointState msg;
+  //  msg.name.push_back("j2n6s300_joint_1");
+  //  msg.position.push_back(rand() %10);
+  //  msg.name.push_back("j2n6s300_joint_2");
+  //  msg.position.push_back(rand() %10);
+    //msg.name = ['j2n6s300_joint_1', 'j2n6s300_joint_2', 'j2n6s300_joint_4', 'j2n6s300_joint_3'];
+    //msg.position = [2, 3.59, 7.01, 3.07];
+
+    chatter_pub.publish(msg);
+    //ROS_INFO("%s",msg.data.c_str());
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+
+  }
+  return 0;
+}
--- src/myPlanner.cpp
+++ src/myPlanner.cpp
@@ -1,5 +1,6 @@
 #include <pluginlib/class_loader.h>
 #include <ros/ros.h>
+#include <mytutorialPkg/HandStampedPose.h>
 
 // MoveIt!
 #include <moveit/robot_model_loader/robot_model_loader.h>
@@ -12,8 +13,24 @@
 #include <boost/scoped_ptr.hpp>
 
 #include <ctime>
-void updatePoseValues(geometry_msgs::PoseStamped& newPose, geometry_msgs::PoseStamped& oldPose){
-  oldPose = newPose;
+
+geometry_msgs::PoseStamped poseTip2;//Right Finger tip
+geometry_msgs::PoseStamped poseLink6;//wrist
+geometry_msgs::PoseStamped poseTip1;//Left Finger tip
+bool messageReceived = false;
+
+// A tolerance of 0.01 m is specified in position
+// and 0.01 radians in orientation
+std::vector<double> tolerance_pose(3, 0.01);
+std::vector<double> tolerance_angle(3, 0.01);
+
+
+void updatePoseValues(const mytutorialPkg::HandStampedPose::ConstPtr& msg){
+  ROS_INFO("I received a message.. now processing...");
+  poseTip2 = msg->poseTip2;
+  poseLink6 = msg->poseLink6;
+  poseTip1 = msg->poseTip1;
+  messageReceived = true;
 }
 
 int main(int argc, char** argv)
@@ -86,119 +103,146 @@ int main(int argc, char** argv)
 
   /* Sleep a little to allow time to startup rviz, etc. */
   ros::WallDuration sleep_time(15.0);
+  ros::WallDuration waitForPlan_time(0.5);
   sleep_time.sleep();
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////
   //---------Loop of the MAIN PROGRAM------------------------------
   //////////////////////////////////////////////////////////////////////////////////////////////////////
-
+  while (ros::ok()){
   //////////////////////////////////////////////////////////////////////////////////////////////////////
   //get input Pose using subscriber--which calls updatesPoseValues
   //Only continue when a new pose is received
   //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  ros::Subscriber sub = node_handle.subscribe("/handPoseTopic",10,updatePoseValues);
+  if(messageReceived){
+    ROS_INFO("Message was Received!");
+  }
+  else{
+    ROS_INFO("Message not Received...");
+  }
   //////////////////////////////////////////////////////////////////////////////////////////////////////
   //Then run the standard planning technique
   //////////////////////////////////////////////////////////////////////////////////////////////////////
-  planning_interface::MotionPlanRequest req;
-  planning_interface::MotionPlanResponse res;
-  planning_interface::MotionPlanRequest req2;
-  planning_interface::MotionPlanResponse res2;
+  if(messageReceived){
+    planning_interface::MotionPlanRequest req;
+    planning_interface::MotionPlanResponse res;
+    planning_interface::MotionPlanRequest req2;
+    planning_interface::MotionPlanResponse res2;
 
-  geometry_msgs::PoseStamped poseTip2;//Right Finger tip---Demo data
-  poseTip2.header.frame_id = "m1n6a200_link_base";
-  poseTip2.pose.position.x = 0.305735;
-  poseTip2.pose.position.y = 0.196963;
-  poseTip2.pose.position.z = 0.663391;
-  poseTip2.pose.orientation.x = 0.623481;
-  poseTip2.pose.orientation.y = 0.3335;
-  poseTip2.pose.orientation.z = 0.623522;
-  poseTip2.pose.orientation.w = 0.333571;
+    /*geometry_msgs::PoseStamped poseTip2;//Right Finger tip---Demo data
+    poseTip2.header.frame_id = "m1n6a200_link_base";
+    poseTip2.pose.position.x = 0.305735;
+    poseTip2.pose.position.y = 0.196963;
+    poseTip2.pose.position.z = 0.663391;
+    poseTip2.pose.orientation.x = 0.623481;
+    poseTip2.pose.orientation.y = 0.3335;
+    poseTip2.pose.orientation.z = 0.623522;
+    poseTip2.pose.orientation.w = 0.333571;
 
-  geometry_msgs::PoseStamped poseLink6;//wrist --- Demo Data
-  poseLink6.header.frame_id = "m1n6a200_link_base";
-  poseLink6.pose.position.x = 0.308219;
-  poseLink6.pose.position.y = 0.0494515;
-  poseLink6.pose.position.z = 0.727438;
-  poseLink6.pose.orientation.x = 0.615034;
-  poseLink6.pose.orientation.y = 0.0;
-  poseLink6.pose.orientation.z = 0.0;
-  poseLink6.pose.orientation.w = 0.788501;
+    geometry_msgs::PoseStamped poseLink6;//wrist --- Demo Data
+    poseLink6.header.frame_id = "m1n6a200_link_base";
+    poseLink6.pose.position.x = 0.308219;
+    poseLink6.pose.position.y = 0.0494515;
+    poseLink6.pose.position.z = 0.727438;
+    poseLink6.pose.orientation.x = 0.615034;
+    poseLink6.pose.orientation.y = 0.0;
+    poseLink6.pose.orientation.z = 0.0;
+    poseLink6.pose.orientation.w = 0.788501;
 
-  geometry_msgs::PoseStamped poseTip1;//Left Finger tip---Demo data
-  poseTip1.header.frame_id = "m1n6a200_link_base";
-  poseTip1.pose.position.x = 0.30573;
-  poseTip1.pose.position.y = 0.211976;
-  poseTip1.pose.position.z = 0.723024;
-  poseTip1.pose.orientation.x = 0.648451;
-  poseTip1.pose.orientation.y = 0.282066;
-  poseTip1.pose.orientation.z = -0.648369;
-  poseTip1.pose.orientation.w = -0.282078;
+    geometry_msgs::PoseStamped poseTip1;//Left Finger tip---Demo data
+    poseTip1.header.frame_id = "m1n6a200_link_base";
+    poseTip1.pose.position.x = 0.30573;
+    poseTip1.pose.position.y = 0.211976;
+    poseTip1.pose.position.z = 0.723024;
+    poseTip1.pose.orientation.x = 0.648451;
+    poseTip1.pose.orientation.y = 0.282066;
+    poseTip1.pose.orientation.z = -0.648369;
+    poseTip1.pose.orientation.w = -0.282078;
 
-  // A tolerance of 0.01 m is specified in position
-  // and 0.01 radians in orientation
-  std::vector<double> tolerance_pose(3, 0.01);
-  std::vector<double> tolerance_angle(3, 0.01);
+    // A tolerance of 0.01 m is specified in position
+    // and 0.01 radians in orientation
+    std::vector<double> tolerance_pose(3, 0.01);
+    std::vector<double> tolerance_angle(3, 0.01);
+  */
+    req.group_name = "chainArm";
+    moveit_msgs::Constraints pose_goal_tip_2 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_finger_tip_2", poseTip2, tolerance_pose, tolerance_angle);
+    req.goal_constraints.push_back(pose_goal_tip_2);
 
-  req.group_name = "chainArm";
-  moveit_msgs::Constraints pose_goal_tip_2 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_finger_tip_2", poseTip2, tolerance_pose, tolerance_angle);
-  req.goal_constraints.push_back(pose_goal_tip_2);
+    moveit_msgs::Constraints pose_goal_link6 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_6", poseLink6, tolerance_pose, tolerance_angle);
+    req.goal_constraints.push_back(pose_goal_link6);
 
-  moveit_msgs::Constraints pose_goal_link6 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_6", poseLink6, tolerance_pose, tolerance_angle);
-  req.goal_constraints.push_back(pose_goal_link6);
+    planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
 
-  planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
+    context->solve(res);
+    if (res.error_code_.val != res.error_code_.SUCCESS){
+      ROS_ERROR("Could not compute plan successfully");
+      return 0;
+    }
 
-  context->solve(res);
-  if (res.error_code_.val != res.error_code_.SUCCESS){
-    ROS_ERROR("Could not compute plan successfully");
-    return 0;
+    //-------------Robot has planned to the first motion------------------------------------
+    moveit_msgs::MotionPlanResponse midResponse;
+    res.getMessage(midResponse);//Get the first motion plan
+
+    robot_state::RobotState& robot_state = planning_scene->getCurrentStateNonConst();
+    planning_scene->setCurrentState(midResponse.trajectory_start);
+    const robot_state::JointModelGroup* joint_model_group = robot_state.getJointModelGroup("chainArm");
+    robot_state.setJointGroupPositions(joint_model_group, midResponse.trajectory.joint_trajectory.points.back().positions);
+
+    req2.group_name = "chainArmLeft";
+    moveit_msgs::Constraints pose_goal_tip_1 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_finger_tip_1", poseTip1, tolerance_pose, tolerance_angle);
+    req2.goal_constraints.push_back(pose_goal_tip_1);
+    req2.goal_constraints.push_back(pose_goal_link6);
+
+    planning_interface::PlanningContextPtr context2 = planner_instance->getPlanningContext(planning_scene, req2, res2.error_code_);
+    context2->solve(res2);
+    if (res2.error_code_.val != res2.error_code_.SUCCESS){
+      ROS_ERROR("Could not compute plan successfully");
+      return 0;
+    }
+
+    moveit_msgs::MotionPlanResponse endResponse;
+    res.getMessage(endResponse);
+
+    robot_state = planning_scene->getCurrentStateNonConst();
+    planning_scene->setCurrentState(endResponse.trajectory_start);
+    joint_model_group = robot_state.getJointModelGroup("chainArmLeft");
+    robot_state.setJointGroupPositions(joint_model_group, endResponse.trajectory.joint_trajectory.points.back().positions);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //show the result in Rviz
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+    moveit_msgs::DisplayTrajectory display_trajectory;
+
+    ROS_INFO("Visualizing the trajectory");
+    moveit_msgs::MotionPlanResponse response;
+
+    res.getMessage(response);//Get the first motion plan
+    display_trajectory.trajectory_start = response.trajectory_start;
+    display_trajectory.trajectory.push_back(response.trajectory);
+
+    res2.getMessage(response);//Get the second motion plan
+    display_trajectory.trajectory_start = response.trajectory_start;
+    display_trajectory.trajectory.push_back(response.trajectory);
+    display_publisher.publish(display_trajectory);
+    waitForPlan_time.sleep();
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Move the physical robot to the planned coordinates
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Reset
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    messageReceived = false;
   }
+  ros::spinOnce();
+  //Maybe add wait here...
 
-  //-------------Robot has planned to the first motion------------------------------------
-  moveit_msgs::MotionPlanResponse midResponse;
-  res.getMessage(midResponse);//Get the first motion plan
-
-  robot_state::RobotState& robot_state = planning_scene->getCurrentStateNonConst();
-  planning_scene->setCurrentState(midResponse.trajectory_start);
-  const robot_state::JointModelGroup* joint_model_group = robot_state.getJointModelGroup("chainArm");
-  robot_state.setJointGroupPositions(joint_model_group, midResponse.trajectory.joint_trajectory.points.back().positions);
-
-  req2.group_name = "chainArmLeft";
-  moveit_msgs::Constraints pose_goal_tip_1 = kinematic_constraints::constructGoalConstraints("m1n6a200_link_finger_tip_1", poseTip1, tolerance_pose, tolerance_angle);
-  req2.goal_constraints.push_back(pose_goal_tip_1);
-  req2.goal_constraints.push_back(pose_goal_link6);
-
-  planning_interface::PlanningContextPtr context2 = planner_instance->getPlanningContext(planning_scene, req2, res2.error_code_);
-  context2->solve(res2);
-  if (res2.error_code_.val != res2.error_code_.SUCCESS){
-    ROS_ERROR("Could not compute plan successfully");
-    return 0;
-  }
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-  //show the result in Rviz
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-  ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-  moveit_msgs::DisplayTrajectory display_trajectory;
-
-  ROS_INFO("Visualizing the trajectory");
-  moveit_msgs::MotionPlanResponse response;
-
-  res.getMessage(response);//Get the first motion plan
-  display_trajectory.trajectory_start = response.trajectory_start;
-  display_trajectory.trajectory.push_back(response.trajectory);
-
-  res2.getMessage(response);//Get the second motion plan
-  display_trajectory.trajectory_start = response.trajectory_start;
-  display_trajectory.trajectory.push_back(response.trajectory);
-  display_publisher.publish(display_trajectory);
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-  //Move the physical robot to the planned coordinates
-  //////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+  waitForPlan_time.sleep();
+}
   //Loop back to start of main Program
 
   // END_TUTORIAL
